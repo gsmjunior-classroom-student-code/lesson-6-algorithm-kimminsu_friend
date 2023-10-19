735357e558d2b9f8f32e2d5adf2e25a1e83f21a2
김밍슈 <104071568+min9-530@users.noreply.github.com>
2023-10-19 10:54:53 +0900

    Create selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+void selection_sort(int arr[], int n) {
+    int i, j, min_idx;
+
+
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selection_sort(arr, n);
+
+    printf("정렬된 배열: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
